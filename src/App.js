01420e52ac71fb80b6c4f1fb95e90de6e1565e8f
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-08-19 16:51:46 -0400

    adding hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
-import Header from "./Header"
-import data from "./"
+import Header from "./components/Header"
+import data from "./data.json"
+import toDoList from "./components/ToDoList"
 
 function App() {
+  const [toDoList, setToDoList]=useState(data);
   return (
     <div className="App">
      <Header/>
+     <toDoList toDoList={toDoList}/>
     </div>
   );
 }
---
+++ src/components/ToDoList.js
@@ -0,0 +1,2 @@
+import React, {useState} from 'react'
+import toDo from "./ToDo"
