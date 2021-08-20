ef2cc69ef7648e7a4aa41f3e5bf31208c3e9530f
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-08-20 00:01:03 -0400

    adding the toggle to complete tasks

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Header from "./components/Header"
 import data from "./data.json"
 import ToDoList from "./components/ToDoList"
 
+
+
 function App() {
   const [toDoList, setToDoList]=useState(data);
 
+  const handleToggle = (id) => {
+      let mapped = toDoList.map(task => {
+        return task.id == id ? { ...task, complete: !task.complete} : {...task};
+      });
+      setToDoList(mapped);
+  }
+
   return (
+    
     <div className="App">
      <Header/>
      <ToDoList toDoList={toDoList}/>
+     
     </div>
   );
 }
